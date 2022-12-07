fdccefb2206e83b0092a0f1777cc6a0981f7bda0
petrus <pshai671@gmail.com>
2022-12-07 14:25:07 +0200

    Add project files.

---
+++ PersonalDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class PersonalDetails
+    {
+        public string Name;
+        public string Surname;
+        public string Monthofbirth;
+        public int yearofbirth;
+        public int Dayofbirth;
+
+        public PersonalDetails (string personName, string personSurname, string personMonthofbirth, int personyearofbirth, int personDayofbirth)
+        {
+            Name = personName;
+            Surname = personSurname;
+            Monthofbirth = personMonthofbirth;
+            yearofbirth = personyearofbirth;
+            Dayofbirth = personDayofbirth;
+
+        }
+            
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Object oriented programming;
+using System.ComponentModel.DataAnnotations;
+
+class program
+{
+    PersonalDetails person1 = new PersonalDetails("PETRUS", "SHAI", "SEPTEMBER", 13, 1999);
+    PersonalDetails person2 = new PersonalDetails("MACHABA", "SHAI", "JANUARY", 10, 1994);
+    PersonalDetails person3 = new PersonalDetails("DINEO", "SHAI", "AUGUST", 16, 2006);
+
+
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(person1.Name + " " + person1.Surname +person1.Dayofbirth + " " + person1.Monthofbirth + person1.yearofbirth);
+        Console.WriteLine(person2.Name + " " + person2.Surname + person2.Dayofbirth + " " + person2.Monthofbirth + person2.yearofbirth);
+        Console.WriteLine(person3.Name + " " + .person3.Surname + person3.Dayofbirth + " " + person3.Monthofbirth + person3.yearofbirth);
+    }
+
+
